28485f2094fc3d7275a4056f574394d4a9928c18
unknown <ebubepeter919@gmail.com>
2026-03-11 19:27:15 +0100

    fix email password

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, getMe, forgotPassword, resetPassword } = require('../controllers/authController');
+const { register, login, getMe, forgotPassword, resetPassword, googleAuth } = require('../controllers/authController');
 const protect = require('../middleware/auth');
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password', resetPassword);
+router.post('/google', googleAuth);
 module.exports = router;
-router.post('/google', require('../controllers/authController').googleAuth);
